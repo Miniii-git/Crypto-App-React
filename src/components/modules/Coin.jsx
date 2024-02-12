1b4aec6874430c6ef8fd028a057bbb9bbcdb928b
Miniii-git <125822323+Miniii-git@users.noreply.github.com>
2024-02-12 09:00:39 +0330

    add css style for table and coin

--- src/components/modules/Coin.jsx
+++ src/components/modules/Coin.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import styles from "./Coin.module.css";
 
 export default function Coin({ coin }) {
   return (
-    <div>
-      <img src={coin.image} alt={coin.name} width="15px" />
-      <span> {coin.symbol}</span>
-
+    <div className={styles.coinInfo}>
+      <div>
+        <img src={coin.image} alt={coin.name} width="15px" />
+        &nbsp; <span>{coin.symbol}</span>
+      </div>
       <p>{coin.name}</p>
       <p>{coin.current_price}</p>
       <p>{coin.price_change_percentage_24h}</p>
       <p>{coin.total_volume}</p>
-      <br />
-      <hr />
-      <br />
     </div>
   );
 }
--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Coin from "./Coin";
+import styles from "./tableCoin.module.css";
 
 function TableCoins({ coinsData }) {
   return (
-    <div>
-      {coinsData.map((coin) => (
-        <Coin key={coin.id} coin={coin} />
-      ))}
-    </div>
+    <>
+      <div className={styles.titleOfTable}>
+        <span>Coin</span>
+        <span>Name</span>
+        <span>Price</span>
+        <span>24h</span>
+        <span>Total Volume</span>
+      </div>
+      <div>
+        {coinsData.map((coin) => (
+          <Coin key={coin.id} coin={coin} />
+        ))}
+      </div>
+    </>
   );
 }
 
